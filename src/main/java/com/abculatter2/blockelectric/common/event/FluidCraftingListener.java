67573633d5163e0980c61ed8e5901c3369d8689a
Tamaized <ryanolt123@gmail.com>
2017-12-01 14:39:17 -0800

    dont need this, forgot to remove it

--- src/main/java/com/abculatter2/blockelectric/common/event/FluidCraftingListener.java
+++ src/main/java/com/abculatter2/blockelectric/common/event/FluidCraftingListener.java
@@ -3,32 +3,21 @@ package com.abculatter2.blockelectric.common.event;
 import com.abculatter2.blockelectric.TheBlockElectric;
 import com.abculatter2.blockelectric.common.fluid.BlockFluidFiniteCrafting;
 import com.abculatter2.blockelectric.common.recipes.MoltenCrafting;
-import com.google.common.collect.Lists;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.init.Items;
-import net.minecraft.item.Item;
 import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-import java.util.List;
-
 @Mod.EventBusSubscriber(modid = TheBlockElectric.MODID)
 public class FluidCraftingListener {
-
-	private static final List<Item> ALLOWED_ITEMS = Lists.newArrayList(
-
-			Items.BLAZE_POWDER
-
-	);
 
 	@SubscribeEvent
 	public static void tick(TickEvent.WorldTickEvent e) {
 		if (e.phase == TickEvent.Phase.START)
 			return;
-		for (EntityItem item : e.world.getEntities(EntityItem.class, input -> input != null && !input.isDead && ALLOWED_ITEMS.contains(input.getItem().getItem()))) {
+		for (EntityItem item : e.world.getEntities(EntityItem.class, input -> input != null && !input.isDead)) {
 			boolean flag = true;
 			for (MoltenCrafting.MoltenRecipe r : MoltenCrafting.RECIPES)
 				if (r.isIngredient(item.getItem())) {
--- src/main/java/com/abculatter2/blockelectric/common/fluid/BlockFluidFiniteCrafting.java
+++ src/main/java/com/abculatter2/blockelectric/common/fluid/BlockFluidFiniteCrafting.java
@@ -2,32 +2,15 @@ package com.abculatter2.blockelectric.common.fluid;
 
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
-import net.minecraft.entity.item.EntityItem;
-import net.minecraft.init.Items;
-import net.minecraft.util.math.AxisAlignedBB;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fluids.BlockFluidFinite;
 import net.minecraftforge.fluids.Fluid;
 
-import javax.annotation.Nonnull;
-import java.util.List;
-import java.util.Random;
-
 public class BlockFluidFiniteCrafting extends BlockFluidFinite {
 
 	public BlockFluidFiniteCrafting(Fluid fluid, Material material) {
 		super(fluid, material);
-	}
-
-	@Override
-	public void updateTick(@Nonnull World world, @Nonnull BlockPos pos, @Nonnull IBlockState state, @Nonnull Random rand) {
-		List<EntityItem> items = world.getEntitiesWithinAABB(EntityItem.class, new AxisAlignedBB(pos, pos.add(1, 1, 1)));
-		for (EntityItem item : items) {
-			if (item.getItem().getItem() == Items.BLAZE_POWDER) {
-				drain(world, pos, state, 1);
-			}
-		}
 	}
 
 	public void drain(World world, BlockPos pos, IBlockState state, int amount) {
